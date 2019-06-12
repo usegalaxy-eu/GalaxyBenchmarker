46c0564543debc631e47764f2e380e8571d4820f
Andreas Skorczyk <me@andreas-sk.de>
2019-06-12 12:34:25 +0200

    Catch HTTP-Error in planemo_bridge

--- galaxy_benchmarker/planemo_bridge.py
+++ galaxy_benchmarker/planemo_bridge.py
@@ -2,7 +2,9 @@
 Bridge between Planemo and GalaxyBenchmarker
 """
 import random
+import bioblend
 import time
+import logging
 from galaxy_bridge import Galaxy
 from destination import PulsarMQDestination
 from planemo import options
@@ -10,6 +12,8 @@ from planemo.cli import Context
 from planemo.engine import engine_context
 from planemo.galaxy.test import handle_reports_and_summary
 from planemo.runnable import for_paths
+
+log = logging.getLogger("GalaxyBenchmarker")
 
 
 def run_planemo(glx: Galaxy, dest: PulsarMQDestination, workflow_path):
@@ -40,10 +44,14 @@ def cli(ctx, paths, glx, user_key, **kwds):
 
     runnables = for_paths(paths)
 
-    with engine_context(ctx, **kwds) as engine:
-        test_data = engine.test(runnables)
-        exit_code = handle_reports_and_summary(ctx, test_data.structured_data, kwds=kwds)
-        status = "success" if exit_code == 0 else "error"
+    try:
+        with engine_context(ctx, **kwds) as engine:
+            test_data = engine.test(runnables)
+            exit_code = handle_reports_and_summary(ctx, test_data.structured_data, kwds=kwds)
+            status = "success" if exit_code == 0 else "error"
+    except bioblend.ConnectionError as err:
+        log.error("There was an error with the connection.")
+        status = "error"
 
     return {"status": status, "history_name": kwds["history_name"]}
 
