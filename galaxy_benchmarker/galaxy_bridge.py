37a81369846d33a8bf409d212c1128a15013e045
Andreas Skorczyk <me@andreas-sk.de>
2019-06-17 19:37:19 +0200

    Bugfix

--- galaxy_benchmarker/galaxy_bridge.py
+++ galaxy_benchmarker/galaxy_bridge.py
@@ -9,9 +9,10 @@ log = logging.getLogger("GalaxyBenchmarker")
 
 
 class Galaxy:
-    def __init__(self, url, admin_key, ssh_user, ssh_key, galaxy_root_path, galaxy_config_dir, galaxy_user):
+    def __init__(self, url, admin_key, shed_install, ssh_user, ssh_key, galaxy_root_path, galaxy_config_dir, galaxy_user):
         self.url = url
         self.admin_key = admin_key
+        self.shed_install = shed_install
         self.ssh_user = ssh_user
         self.ssh_key = ssh_key
 
@@ -36,17 +37,17 @@ class Galaxy:
         Creates a new user (if not already created) with username and a random password and returns
         its user_id and api_key
         """
-        new_user = False
         if len(self.instance.users.get_users(f_name=username)) == 0:
             password = ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(32)])
             self.instance.users.create_local_user(username,
                                                   "{username}@galaxy.uni.andreas-sk.de".format(username=username),
                                                   password)
-            new_user = True
 
         user_id = self.instance.users.get_users(f_name=username)[0]["id"]
-        user_key = self.instance.users.create_user_apikey(user_id) if new_user \
-            else self.instance.users.get_user_apikey(user_id)
+        user_key = self.instance.users.get_user_apikey(user_id)
+
+        if user_key == "Not available.":
+            user_key = self.instance.users.create_user_apikey(user_id)
 
         return user_id, user_key
 
