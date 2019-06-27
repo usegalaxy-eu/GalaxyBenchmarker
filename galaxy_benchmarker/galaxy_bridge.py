76baa82428d04ec2b11cf85f6b3aaf802b97609e
Andreas Skorczyk <me@andreas-sk.de>
2019-06-27 12:26:46 +0200

    Added some more comments to galaxy_bridge.py

--- galaxy_benchmarker/galaxy_bridge.py
+++ galaxy_benchmarker/galaxy_bridge.py
@@ -1,4 +1,5 @@
 from bioblend.galaxy import GalaxyInstance
+from typing import Tuple
 import ansible_bridge
 import logging
 import string
@@ -22,7 +23,7 @@ class Galaxy:
 
         self.instance = GalaxyInstance(url, key=admin_key)
 
-    def impersonate(self, user=None, user_key=None):
+    def impersonate(self, user=None, user_key=None) -> GalaxyInstance:
         """
         Returns a GalaxyInstance for the given user_key. If user is provided,
         user_key is fetched from Galaxy.
@@ -32,10 +33,10 @@ class Galaxy:
             user_key = self.instance.users.get_user_apikey(user_id)
         return GalaxyInstance(self.url, key=user_key)
 
-    def create_user(self, username):
+    def create_user(self, username) -> Tuple:
         """
         Creates a new user (if not already created) with username and a random password and returns
-        its user_id and api_key
+        its user_id and api_key as a tuple.
         """
         if len(self.instance.users.get_users(f_name=username)) == 0:
             password = ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(32)])
@@ -52,6 +53,10 @@ class Galaxy:
         return user_id, user_key
 
     def deploy_job_conf(self):
+        """
+        Deploys the job_conf.xml-file to the Galaxy-Server.
+        """
+        # Hostname parsed from the Galaxy-URL
         host = re.findall("^[a-z][a-z0-9+\-.]*://([a-z0-9\-._~%!$&'()*+,;=]+@)?([a-z0-9\-._~%]+|\[[a-z0-9\-."
                           + "_~%!$&'()*+,;=:]+\])", self.url)[0][1]
         values = {
