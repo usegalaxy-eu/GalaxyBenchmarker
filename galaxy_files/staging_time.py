7b44b7e953b710bc2e4e6109ed0ce7324595452e
Andreas Skorczyk <me@andreas-sk.de>
2019-08-07 17:08:38 +0200

    Staging_Time Plugin

---
+++ galaxy_files/staging_time.py
@@ -0,0 +1,37 @@
+"""The module describes the ``uname`` job metrics plugin."""
+from . import InstrumentPlugin
+from .. import formatting
+import os.path
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class StagingTimeFormatter(formatting.JobMetricFormatter):
+
+    def format(self, key, value):
+        return key, value
+
+
+class StagingTimePlugin(InstrumentPlugin):
+    """ Use uname to gather operating system information about remote system
+    job is running on. Linux only.
+    """
+    plugin_type = "staging_time"
+    formatter = StagingTimeFormatter()
+
+    def __init__(self, **kwargs):
+        pass
+
+    def job_properties(self, job_id, job_directory):
+        result = {}
+
+        preprocess_time_path = self._instrument_file_path(job_directory, "preprocessing_time")
+        if os.path.isfile(preprocess_time_path):
+            with open(preprocess_time_path) as f:
+                result["preprocess_time"] = f.read()
+
+        return result
+
+
+__all__ = ('StagingTimePlugin', )
