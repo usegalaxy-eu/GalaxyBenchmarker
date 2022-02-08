7ee8152f101f710db7bbaec9b769acae3f5df144
smoehrle <26185187+smoehrle@users.noreply.github.com>
2022-02-08 17:26:50 +0100

    Setup irods only on the first startup

--- playbooks/files/irods/scripts/entrypoint.sh
+++ playbooks/files/irods/scripts/entrypoint.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-echo Installing iRODS...
 
-python /var/lib/irods/scripts/setup_irods.py --json_configuration_file /scripts/server_config.json
-
-if [ $? -eq 0 ]
-then
-  echo Installation done!
+if [ -f "/.irods_installed" ]; then
+    echo iRODS already installed. Skipping Installation
 else
-  echo "setup_irods failed"
-  exit 1
+    echo Installing iRODS...
+
+    python /var/lib/irods/scripts/setup_irods.py --json_configuration_file /scripts/server_config.json
+
+    if [ $? -eq 0 ]; then
+        echo Installation done!
+        touch /.irods_installed
+    else
+        echo "setup_irods failed"
+        exit 1
+    fi
 fi
 
 echo Starting server as user 'irods'...
