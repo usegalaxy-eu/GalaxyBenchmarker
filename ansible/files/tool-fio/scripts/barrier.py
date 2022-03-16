2370019fab2728ed196f130915a20feb8d79c6fc
smoehrle <26185187+smoehrle@users.noreply.github.com>
2022-03-16 15:15:46 +0100

    Cleanup and fix containerized fio

--- ansible/files/tool-fio/scripts/barrier.py
+++ ansible/files/tool-fio/scripts/barrier.py
@@ -4,8 +4,13 @@ import socket
 import time
 
 def server(host, port, num_clients):
+    """Start a server which waits for num_clients connections and syncs the clients
+
+    When num_clients connections connected, all will receive "go!"
+    """
     start = time.monotonic()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(20.0)
         s.bind((host, port))
         s.listen(num_clients)
         print(f"Server started. Accepting clients.")
@@ -14,17 +19,19 @@ def server(host, port, num_clients):
         connections = []
         for i in range(num_clients):
             connections.append(s.accept())
-            print(f"{i} connected after {time.monotonic()-start}")
+            print(f"{i} connected after {time.monotonic() - start}")
 
         # All clients connected and are waiting
         for conn, _ in connections:
             conn.sendall(b'go!')
             conn.close()
 
-    print(f"Server stopped after {time.monotonic - start}")
+    print(f"Server stopped after {time.monotonic() - start}")
 
 def client(host, port):
+    """Connect to the sync server and wait for the 'go!' signal"""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(20.0)
         s.connect((host, port))
         print("Client waiting")
 
@@ -32,6 +39,10 @@ def client(host, port):
         print("Client running")
 
 if __name__ == "__main__":
+    """Usage:
+    Start Server: [host] [port] [num_clients]
+    Start Client: [host] [port]
+    """
     print(sys.argv)
     if len(sys.argv) == 4:
         _, host, port, num_clients = sys.argv
