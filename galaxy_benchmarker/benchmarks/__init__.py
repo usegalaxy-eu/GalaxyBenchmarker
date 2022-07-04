f74f7483b814797ba99ec66ae2906249935c671a
smoehrle <26185187+smoehrle@users.noreply.github.com>
2022-07-04 18:33:49 +0200

    Add BenchmarkCompare for comparison of benchmark

--- galaxy_benchmarker/benchmarks/__init__.py
+++ galaxy_benchmarker/benchmarks/__init__.py
@@ -3,3 +3,4 @@ from galaxy_benchmarker.benchmarks.dd import *
 from galaxy_benchmarker.benchmarks.fio import *
 from galaxy_benchmarker.benchmarks.mdtest import *
 from galaxy_benchmarker.benchmarks.s3benchmark import *
+from galaxy_benchmarker.benchmarks.utils import *
