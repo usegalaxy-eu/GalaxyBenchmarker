74fb3d46c70d9fd97f7e3ed5e8f3ede54dff0387
smoehrle <26185187+smoehrle@users.noreply.github.com>
2022-07-05 13:46:25 +0200

    Change dd parallel 2->4

--- ansible/files/tool-dd/entrypoint.sh
+++ ansible/files/tool-dd/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 dd bs=$DD_BS if=$DD_IF of=$DD_OF conv=fsync count=$DD_COUNT &
-dd bs=$DD_BS if=$DD_IF of=$DD_OF conv=fsync count=$DD_COUNT seek=$DD_SEEK skip=$DD_SKIP &
+dd bs=$DD_BS if=$DD_IF of=$DD_OF conv=fsync count=$DD_COUNT seek=$DD_SEEK_1 skip=$DD_SKIP_1 &
+dd bs=$DD_BS if=$DD_IF of=$DD_OF conv=fsync count=$DD_COUNT seek=$DD_SEEK_2 skip=$DD_SKIP_2 &
+dd bs=$DD_BS if=$DD_IF of=$DD_OF conv=fsync count=$DD_COUNT seek=$DD_SEEK_3 skip=$DD_SKIP_3 &
 wait
