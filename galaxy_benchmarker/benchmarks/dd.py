31f8be95c969b58fb8ba3322a5d7bd82ce3bcbf8
smoehrle <26185187+smoehrle@users.noreply.github.com>
2022-07-05 14:27:11 +0200

    Fix os.rename error

--- galaxy_benchmarker/benchmarks/dd.py
+++ galaxy_benchmarker/benchmarks/dd.py
@@ -7,6 +7,7 @@ import dataclasses
 import logging
 import re
 import time
+import shutil
 from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
@@ -112,7 +113,7 @@ class DdFixedParams(base.Benchmark):
         if self.benchmarker.config.results_save_raw_results:
             new_path = self.benchmarker.results / self.result_file.stem
             new_path.mkdir(exist_ok=True)
-            result_file.rename(new_path / result_file.name)
+            shutil.move(result_file, new_path / result_file.name)
 
         log.info("Run took %d s", total_runtime)
 
