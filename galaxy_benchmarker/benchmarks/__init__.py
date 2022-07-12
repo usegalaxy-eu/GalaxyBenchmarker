e3eab1a418869f124a696e80df32bddebeb42766
smoehrle <26185187+smoehrle@users.noreply.github.com>
2022-07-12 14:50:42 +0200

    Prepare second experiment runs

--- galaxy_benchmarker/benchmarks/__init__.py
+++ galaxy_benchmarker/benchmarks/__init__.py
@@ -3,4 +3,5 @@ from galaxy_benchmarker.benchmarks.dd import *
 from galaxy_benchmarker.benchmarks.fio import *
 from galaxy_benchmarker.benchmarks.mdtest import *
 from galaxy_benchmarker.benchmarks.s3benchmark import *
+from galaxy_benchmarker.benchmarks.warp import *
 from galaxy_benchmarker.benchmarks.utils import *
